3222929c5019cdf290cc1b97d7bacb4180ff2f11
B12Howard <howard.luong87@gmail.com>
2022-06-05 20:54:37 -0700

    feat(index): add Store

--- src/index.tsx
+++ src/index.tsx
@@ -7,16 +7,19 @@ import { BrowserRouter } from 'react-router-dom';
 import { register as registerServiceWorker } from './serviceWorkerRegistration';
 import AppRoutes from './Views/Routes';
 import { HelmetProvider } from 'react-helmet-async';
+import Store from './Store/Store';
 // import Offline from './Shared/Components/Offline/offline';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <HelmetProvider>
-            <BrowserRouter>
-                <AppRoutes />
-            </BrowserRouter>
-        </HelmetProvider>
-    </React.StrictMode>,
+    <Store>
+        <React.StrictMode>
+            <HelmetProvider>
+                <BrowserRouter>
+                    <AppRoutes />
+                </BrowserRouter>
+            </HelmetProvider>
+        </React.StrictMode>
+    </Store>,
     document.getElementById('root')
 );
 
