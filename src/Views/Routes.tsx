b71b3f8f45678789c388136ff860c217ed14bc5b
B12Howard <howard.luong87@gmail.com>
2022-06-05 21:16:03 -0700

    feat(Routes): nest routes to use parent components

--- src/Views/Routes.tsx
+++ src/Views/Routes.tsx
@@ -1,6 +1,10 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import GifCreator from '../Components/GifCreator/gif-creator';
+import MyUploads from '../Components/GifCreator/my-uploads';
 import LoadingScreen from '../Components/loading-screen';
+import Login from '../Components/Login/login';
+import LoginLayout from '../Components/views/login-layout';
 // import { PlaylistPresentation } from '../Components/Playlist/playlist-presentation';
 // import DashboardLayout from '../Components/views/dashboard-layout';
 // import Dashboard from '../Components/views/dashboard';
@@ -18,10 +22,19 @@ const PlaylistPresentation = Loadable(lazy(() => import('../Components/Playlist/
 
 const AppRoutes = () => (
     <Routes>
+        <Route path="auth" element={<LoginLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<Login />} />
+        </Route>
         <Route path="home" element={<DashboardLayout />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="playlist" element={<PlaylistPresentation />} />
             <Route path="*" element={<DashboardLayout />} />
+        </Route>
+        <Route path="members" element={<DashboardLayout />}>
+            <Route path="gif-creator" element={<GifCreator />} />
+            <Route path="my-uploads" element={<MyUploads />} />
+            <Route path="*" element={<GifCreator />} />
         </Route>
         <Route path="*" element={<DashboardLayout />} />
     </Routes>
