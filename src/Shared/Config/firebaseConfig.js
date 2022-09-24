48ca5d5867deeb233a3475ada7afe1ebbfa89dff
Howard Luong <howard.luong87@gmail.com>
2022-09-24 11:16:20 -0700

    fix(gitignore): add firebaseConfig (#27)

--- src/Shared/Config/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: 'AIzaSyCH2hMF8NqE59Ts7hD9GmIvOY4xPjsZ0EQ',
-    authDomain: 'gifcreator-891cc.firebaseapp.com',
-    projectId: 'gifcreator-891cc',
-    storageBucket: 'gifcreator-891cc.appspot.com',
-    messagingSenderId: '497823342332',
-    appId: '1:497823342332:web:9c8f6506469f66ef6ac179',
-    measurementId: 'G-R5E05W3PVB',
-};
